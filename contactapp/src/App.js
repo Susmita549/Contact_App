7335fc441856bddbfa175a24f55ab55d187c3abe
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-06-20 00:49:05 +0530

    complete

--- contactapp/src/App.js
+++ contactapp/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer/>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/add" element={<h1>I am add comonent</h1>}/>
+        <Route path="/edit/:id" element={<h1>I am edit component</h1>}/>
+        
+        
+      </Routes>
+      
+     
     </div>
   );
 }
